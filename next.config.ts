b6be136ebaaa23d34148a0ca019fe4912bacd1bb
ILFforever <Hammymukura@gmail.com>
2026-02-28 05:46:17 +0700

    fix

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // Increase body size limit for API routes (file uploads)
-  experimental: {
-    bodySizeLimit: '500mb',
-  },
+  /* config options here */
 };
 
 export default nextConfig;
